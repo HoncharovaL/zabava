9b07e9922ba7e5814e96e291a07e905328d55553
Vera Goncharova <uerka1991@gmail.com>
2017-04-14 18:10:59 +0300

    header and footer

---
+++ src/AppBundle/Twig/Extension/NurseryExtension.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use AppBundle\Entity\Nursery;
+
+/**
+ * Class NurseryExtension
+ */
+class NurseryExtension extends \Twig_Extension
+{
+    /**
+     * @var Registry
+     */
+    private $doctrine;
+
+    /**
+     * @var Nursery
+     */
+    private $nursery;
+
+    /**
+     * @param Registry $doctrine
+     */
+    public function __construct(Registry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_Function('get_nursery_phonenumbers', [$this, 'getPhoneNumbers']),
+            new \Twig_Function('get_nursery_owner', [$this, 'getOwner']),
+            new \Twig_Function('get_nursery_email', [$this, 'getEmail']),
+            new \Twig_Function('get_nursery_address', [$this, 'getAddress']),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getPhoneNumbers()
+    {
+        $this->findNursery();
+
+        return [$this->nursery->getPhone(), $this->nursery->getCellPhone()];
+    }
+
+    public function getEmail()
+    {
+        $this->findNursery();
+
+        return $this->nursery->getEmail();
+    }
+
+    public function getOwner()
+    {
+        $this->findNursery();
+
+        return $this->nursery->getOwner();
+    }
+
+    public function getAddress()
+    {
+        $this->findNursery();
+
+        return $this->nursery->getAdress();
+    }
+
+    private function findNursery()
+    {
+        if ($this->nursery === null) {
+            $nurseries = $this->doctrine->getRepository('AppBundle:Nursery')->findAll();
+            $this->nursery = $nurseries[0];
+        }
+    }
+}
